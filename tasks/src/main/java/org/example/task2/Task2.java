7d6d645925a155724cefff585a86c1c28357c964
maxosad <maxosad@mail.ru>
2024-04-20 16:46:33 +0300

    positionCode method

--- tasks/src/main/java/org/example/task2/Task2.java
+++ tasks/src/main/java/org/example/task2/Task2.java
@@ -9,15 +9,24 @@ import java.util.Scanner;
 public class Task2 {
 
     public final double EPS = 1e-39;
+
     public double distanceBetweenDots(double x1, double y1, double x2, double y2) {
         return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
     }
 
-    public static void main(String[] args)  {
+    public int positionCode(double distance, double radius) {
+        if (distance < radius - EPS) {
+            return 1;
+        } else if (distance > radius + EPS) {
+            return 2;
+        }
+        return 0;
 
+    }
 
+    public static void main(String[] args) {
         Path circleFilePath = Paths.get(args[0]);
-        Path dotFilePath = Paths.get( args[1]);
+        Path dotFilePath = Paths.get(args[1]);
         System.out.println(circleFilePath.toString());
         System.out.println(dotFilePath.toString());
         Task2 task2 = new Task2();
@@ -30,20 +39,12 @@ public class Task2 {
             double centerY = circleScanner.nextDouble();
             double radius = circleScanner.nextDouble();
             double distance;
-            while(dotScanner.hasNext()) {
+            while (dotScanner.hasNext()) {
                 double x = dotScanner.nextDouble();
                 double y = dotScanner.nextDouble();
 
                 distance = task2.distanceBetweenDots(x, y, centerX, centerY);
-
-                if (distance < radius - task2.EPS) {
-                    System.out.println(1);
-                } else if (distance > radius + task2.EPS) {
-                    System.out.println(2);
-                } else {
-                    System.out.println(0);
-                }
-
+                System.out.println(task2.positionCode(distance, radius));
             }
 
         } catch (IOException e) {
---
+++ tasks/src/test/java/org/example/task2/Task2Test.java
@@ -0,0 +1,53 @@
+package org.example.task2;
+
+import org.example.task1.Task1;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Task2Test {
+
+    private static Stream<Arguments> provideCoordinatesForCountDistance() {
+
+        return Stream.of(
+                Arguments.of(
+                        Math.sqrt(2),
+                        0, 0,
+                        1, 1
+                ),
+                Arguments.of(
+                        2,
+                        0, 1,
+                        0, -1
+                )
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("count distance tests should finish successfully")
+    @MethodSource("provideCoordinatesForCountDistance")
+    void countDistanceBetweenDots(double expected, double x1, double y1, double x2, double y2) {
+        Task2 task2 = new Task2();
+
+        var result = task2.distanceBetweenDots(x1, y1, x2, y2);
+
+        assertEquals(expected, result);
+    }
+
+
+    @Test
+    @DisplayName("generate 100 dots in circle")
+    void dotsInCircle(double expected, double x1, double y1, double x2, double y2) {
+        Task2 task2 = new Task2();
+
+        var result = task2.distanceBetweenDots(x1, y1, x2, y2);
+
+        assertEquals(expected, result);
+    }
+}
